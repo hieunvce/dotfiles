ae7fb13083da81ec865917113a900cd2a4c0f130
Hieu Nguyen <hieunv.ce@gmail.com>
2024-08-22 14:06:37 +0700

    change nerd font

--- polybar/.config/polybar/launch_polybar.sh
+++ polybar/.config/polybar/launch_polybar.sh
@@ -1,7 +1,7 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload toph &
+    MONITOR=$m polybar -c $HOME/.config/polybar/config.ini --reload toph &
   done
 else
-  polybar --reload toph &
+  polybar -c $HOME/.config/polybar/config.ini --reload toph &
 fi
